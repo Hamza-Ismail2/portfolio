cfeebbafb6047942c497d7ac22f8ea2d9715ccc1
Hamza-Ismail2 <syed.naqvi202@gmail.com>
2025-04-18 01:51:51 +0500

    Updated home page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "portfolio-hamza-ismail2s-projects.vercel.app",
+    template: "%s | portfolio-hamza-ismail2s-projects.vercel.app",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "portfolio-hamza-ismail2s-projects.vercel.app",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://portfolio-hamza-ismail2s-projects.vercel.app.com",
+    siteName: "portfolio-hamza-ismail2s-projects.vercel.app.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://portfolio-hamza-ismail2s-projects.vercel.app",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Hamza-Ismail",
     card: "summary_large_image",
   },
   icons: {
